2371e52f3a80abc89be2378277d51ce9f2a4dd98
puranjanprithu <puranjanprithu@gmail.com>
2020-07-20 16:00:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shield/HomeActivity.java
@@ -0,0 +1,60 @@
+package com.example.shield;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.google.firebase.auth.FirebaseAuth;
+import com.test.shield.DomesticActivity;
+import com.test.shield.HelpActivity;
+import com.test.shield.TraffickingActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    Button btnLogout, trafik,domvol;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        btnLogout = findViewById(R.id.help);
+        trafik=findViewById(R.id.trafficking);
+        domvol=findViewById(R.id.domvol);
+
+        /**btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intToMain);
+            }
+        });**/
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(HomeActivity.this, HelpActivity.class);
+                startActivity(intToMain);
+            }
+        });
+        trafik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(HomeActivity.this, TraffickingActivity.class);
+                startActivity(intToMain);
+            }
+        });
+        domvol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain = new Intent(HomeActivity.this, DomesticActivity.class);
+                startActivity(intToMain);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/test/shield/TraffickingActivity.java
@@ -0,0 +1,16 @@
+package com.test.shield;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.shield.R;
+
+public class TraffickingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trafficking);
+    }
+}
